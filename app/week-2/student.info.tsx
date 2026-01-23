2d3c2da411d139daa509decbddcce96d3e3eb84b
karan1832002 <karandeepsingh2@edu.sait.ca>
2026-01-22 21:04:13 -0700

    fix student info

---
+++ app/week-2/student.info.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Name: Karandeep Singh</p>
+      <Link href="https://github.com/karan1832002/cprg306-assignments.git">
+        https://github.com/karan1832002/cprg306-assignments.git
+      </Link>
+    </div>
+  );
+}
